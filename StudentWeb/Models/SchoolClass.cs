9fcb9d4aa3f222c6b3730cb7342da0de2a021465
woodsun <woodsun@tom.com>
2019-04-28 22:32:58 +0800

    1.0

---
+++ StudentWeb/Models/SchoolClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentWeb.Models
+{
+    public class SchoolClass
+    {
+        public int ID { get; set; }
+
+        public string ClassTitle { get; set; }
+
+        public List<Student> Students { get; set; }
+
+        //班长
+        public Student ClassMonitor { get; set; }
+
+        //语文课代表
+        public Student Chinese { get; set; }
+
+        //数学课代表
+        public Student Mathematics { get; set; }
+    }
+
+}
---
+++ StudentWeb/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentWeb.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public SchoolClass MyClass { get; set; }
+    }
+}
